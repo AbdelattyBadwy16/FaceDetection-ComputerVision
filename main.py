1e3eae72d2c864644cf9b9032ddb2d0277b24429
abdelattybadwy166 <abdelattybadwy166@gmail.com>
2025-03-31 19:37:14 +0200

    feat(smile): add smile Detection

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 
 face_detect = cv2.CascadeClassifier("/home/glitch00/data/Projects/Computer Vision/Face Detection/haarcascade_frontalface_default.xml")
 eye_detect = cv2.CascadeClassifier("/home/glitch00/data/Projects/Computer Vision/Face Detection/haarcascade_eye.xml")
+smile_detect = cv2.CascadeClassifier("/home/glitch00/data/Projects/Computer Vision/Face Detection/haarcascade_smile.xml")
 
 stream = cv2.VideoCapture(0)
 while True : 
@@ -9,8 +10,7 @@ while True :
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
     ##### Face Detect
-
-    faces = face_detect.detectMultiScale(gray,1.3,2)
+    faces = face_detect.detectMultiScale(gray,1.3,5)
     for (x,y,w,h) in faces :
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
         face =  frame[y:y+h,x:x+w]
@@ -18,6 +18,10 @@ while True :
         eyes = eye_detect.detectMultiScale(face)
         for (ex,ey,ew,eh) in eyes : 
             cv2.rectangle(face,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
+        #### smile Detect
+        smile = smile_detect.detectMultiScale(face,1.3,30)
+        for (sx,sy,sw,sh) in smile : 
+            cv2.rectangle(face,(sx,sy),(sx+sw,sy+sh),(255,0,0),2)
 
 
     ##### Show
