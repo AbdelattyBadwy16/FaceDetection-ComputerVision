89f8744a9de9dea191efc3d9bfebd82a6e6b26e0
abdelattybadwy166 <abdelattybadwy166@gmail.com>
2025-03-31 19:31:17 +0200

    feat(eye): add eye Detection

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import cv2
 
 face_detect = cv2.CascadeClassifier("/home/glitch00/data/Projects/Computer Vision/Face Detection/haarcascade_frontalface_default.xml")
+eye_detect = cv2.CascadeClassifier("/home/glitch00/data/Projects/Computer Vision/Face Detection/haarcascade_eye.xml")
 
 stream = cv2.VideoCapture(0)
 while True : 
     st,frame = stream.read()
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    ##### Face Detect
+
     faces = face_detect.detectMultiScale(gray,1.3,2)
     for (x,y,w,h) in faces :
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-    cv2.imshow("vid",frame)
+        face =  frame[y:y+h,x:x+w]
+        #### eye Detect
+        eyes = eye_detect.detectMultiScale(face)
+        for (ex,ey,ew,eh) in eyes : 
+            cv2.rectangle(face,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
+
+
+    ##### Show
+    cv2.imshow("Live Stream",frame)
     cv2.waitKey(33)
